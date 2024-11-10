81d34fe09cc8c4382c5329e32191e22c3feb2dcd
rxmercy <ramielmorsimd@gmail.com>
2024-11-09 23:05:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,26 @@ st.title('soft tissue Sarcoma Reconstruction Nomogram (SaRcoN')
 st.info('This is an tool to predict reconstructive outcomes in limb-sparing extremity soft tissue sarcoma surgery')
 
 import streamlit as st
+import Orange
 import pickle
 import numpy as np
 
 # Load all models
 models = {
-    "minor": pickle.load(open("minor.pkcls", "rb")),
-    "major": pickle.load(open("major.pkcls", "rb")),
-    "ssi": pickle.load(open("ssi.pkcls", "rb")),
-    "id": pickle.load(open("id.pkcls", "rb")),
-    "seroma": pickle.load(open("seroma.pkcls", "rb"))
+with open("minor.pkcls","rb") as minor:
+    minor = pickle.load(minor)
+
+with open("major.pkcls","rb") as major:
+    major = pickle.load(major)
+
+with open("ssi.pkcls","rb") as ssi:
+    ssi = pickle.load(ssi)
+
+with open("id.pkcls","rb") as id:
+    id = pickle.load(id)
+
+with open("seroma.pkcls","rb") as seroma:
+    seroma = pickle.load(seroma)
 }
 
 # Define input options for categorical variables
